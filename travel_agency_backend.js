aa8f146516553a5e07128718434ef2edb303ed69
Sukesh <sukeshawh@gmail.com>
2026-05-26 21:44:54 +0530

    public added

--- travel_agency_backend.js
+++ travel_agency_backend.js
@@ -17,6 +17,7 @@ const rateLimit = require('express-rate-limit');
 const path = require('path');
 const constants = require('./constants.js');
 
+
 // Load environment variables
 dotenv.config();
 
@@ -33,7 +34,8 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname)));
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rate limiting - prevent spam submissions
 const enquiryLimiter = rateLimit({
@@ -517,9 +519,9 @@ app.get('/api/health', (req, res) => {
  * GET /
  * Serve the website homepage
  */
-app.get('/', (req, res) => {
+/* app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'travel_agency_website.html'));
-});
+}); */
 
 /**
  * 404 Handler
